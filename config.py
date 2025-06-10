413e4479b5120ed20ac7cc3fc38f54f7893efa53
bc9z <younessayf764@gmail.com>
2025-06-10 15:31:31 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+EMBED_CONFIG = {
+    "title": "EL HA9 MCHA IS HERE",    # Maine Embed Title here 
+    "description": "EL HA9 MCHA IS HERE",   # Main Embed Description here 
+    "color": 0xFF5733,   # Change embed color if you want (red)
+    "fields": [
+        {"name": "EL HA9 MCHA IS HERE", "value": "EL HA9 MCHA IS HERE", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
+        {"name": "EL HA9 MCHA IS HERE", "value": "EL HA9 MCHA IS HERE", "inline": False},
+        {"name": "EL HA9 MCHA IS HERE","value": "EL HA9 MCHA IS HERE", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
+    ],
+    "image": "",   # Embed Icon url here → https://image.jpg
+    "footer": "",  # Embed Footer here 
+}
+
+SERVER_CONFIG = {
+    "new_name": "EL HA9 MCHA IS HERE",  # New Server Name here 
+    "new_icon": "",   # New Server Icon url here → https://image.jpg 
+    "new_description": "",  # New Server Description here 
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "Nitcha",  # Webhook Name here 
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 15,  # Number of channels
+    'channel_type': 'text',  # text/voice
+    'channel_name': 'EL HA9 MCHA IS HERE',  # Channel name
+    'num_messages': 5,  # Number of message to spam
+    'message_content': 'EL HA9 MCHA IS HERE' # Spam Message
+}
+
+NO_BAN_KICK_ID = {
+    1308142665749237771,       # Put Whitelist ID
+    ,       # No Ban & No Kick
+    ,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",  # "playing", "listening", or "watching"
+    "text": "ElHa9Mcha é Nitcha"  # Your text presence
+}
